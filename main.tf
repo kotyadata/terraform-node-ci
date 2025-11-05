e904e4924fd6ed225cd6c9ebee211737e2491567
kotyadata <kotyadatejaa@gmail.com>
2025-11-05 16:44:32 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "allow_http" {
@@ -16,13 +16,13 @@ resource "aws_security_group" "allow_http" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1"    
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "node_app" {
-  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2
+  ami           = "ami-0bdd88bd06d16ba03" # Amazon Linux 2
   instance_type = "t2.micro"
   key_name      = "jenkins_ec2_key"       # Optional
 
